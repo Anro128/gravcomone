69e94f9c6c4c624fba8880fd975ea786a94129ea
hazelnutxwho <132769330+hazelnutxwho@users.noreply.github.com>
2024-05-15 15:58:34 +0700

    add pie chart

--- script.js
+++ script.js
@@ -57,6 +57,78 @@ function createChart(labels, values) {
         }
     });
 
+    //Pie Chart Pulau Sumatra
+    const pieData1 = [{
+        labels: labels. slice(1,8), // Pie chart labels
+        values: values[1]. slice(1,8), // Pie chart values
+        type: 'pie'
+        }];
+    
+    const layout1 = {
+        font: {size: 18},
+        responsive: true
+        };
+    
+    Plotly.newPlot('PiePlot1', pieData1, layout1);
+
+    
+    //Pie Chart Pulau Jawa
+    const pieData2 = [{
+        labels: labels. slice(11,16), // Pie chart labels
+        values: values[1]. slice(11,16), // Pie chart values
+        type: 'pie'
+        }];
+    
+    const layout2 = {
+        font: {size: 18},
+        responsive: true
+        };
+    
+    Plotly.newPlot('PiePlot2', pieData2, layout2);
+
+
+    //Pie Chart Pulau Kalimantan
+    const pieData3 = [{
+        labels: labels. slice(20,24), 
+        values: values[1]. slice(20,24), 
+        type: 'pie'
+        }];
+    
+    const layout3 = {
+        font: {size: 18},
+        responsive: true
+        };
+    
+    Plotly.newPlot('PiePlot3', pieData3, layout3);
+
+    //Pie Chart Pulau Sulawesi
+    const pieData4 = [{
+        labels: labels. slice(25,30), 
+        values: values[1]. slice(25,30), 
+        type: 'pie'
+        }];
+    
+    const layout4 = {
+        font: {size: 18},
+        responsive: true
+        };
+    
+    Plotly.newPlot('PiePlot4', pieData4, layout4);
+
+    //Pie Chart Pulau Papua
+    const pieData5 = [{
+        labels: labels. slice(33,38), 
+        values: values[1]. slice(33,38), 
+        type: 'pie'
+        }];
+    
+    const layout5 = {
+        font: {size: 18},
+        responsive: true
+        };
+    
+    Plotly.newPlot('PiePlot5', pieData5, layout5);
+
 }
 
 fetchCSVandPlot('DataPadi.csv');
